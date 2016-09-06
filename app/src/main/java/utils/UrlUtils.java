efcfd66dc8345c8958c88ed1e2481216521d24e0
mhysa <mhysa123@hotmail.com>
2016-09-06 09:03:30 +0800

    首页接口调用

--- app/src/main/java/utils/UrlUtils.java
+++ app/src/main/java/utils/UrlUtils.java
@@ -7,7 +7,7 @@ import android.os.Environment;
  */
 public class UrlUtils {
     public static String ImageCachePath = Environment.getExternalStorageDirectory().getPath()+"/BigHealt/ImageCache/";// sd路径
-    public  static String   BASEURL="http://192.168.0.30:8080/JianKangChanYe";
+    public  static String   BASEURL="http://192.168.0.38:8080/JianKangChanYe";
     //-------------------------------API接口----------------------------------
     public static String CommonHome=BASEURL+"/homepictures/sickness";
     public static String SpecialHome = BASEURL+"homepictures/showhealthknowledge";
