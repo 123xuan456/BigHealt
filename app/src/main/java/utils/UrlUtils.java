e9bcbff145d2e0d93d899f0fc81c60fe578fb68d
xuan123 <1044072796@qq.com>
2016-09-08 15:07:51 +0800

    轩：UrlUtils和清单配置

--- app/src/main/java/utils/UrlUtils.java
+++ app/src/main/java/utils/UrlUtils.java
@@ -7,12 +7,21 @@ import android.os.Environment;
  */
 public class UrlUtils {
     public static String ImageCachePath = Environment.getExternalStorageDirectory().getPath()+"/BigHealt/ImageCache/";// sd路径
-    public  static String   BASEURL="http://192.168.0.38:8080/JianKangChanYe";
-    public  static String   LOGIN="http://192.168.0.38:8080/JianKangChanYe/user/login.do?";
-    public  static String   REGISTER_CODE="http://192.168.0.38:8080/JianKangChanYe/user/register?";
-    public  static String   REGISTER="http://192.168.0.38:8080/JianKangChanYe/user/regiontoamaster?";
+    public  static String   BASEURL="http://showtest.dancw.com";
+
+    public static String URL="http://192.168.0.43:8080/";
     //-------------------------------API接口----------------------------------
     public static String CommonHome=BASEURL+"/homepictures/sickness";
     public static String SpecialHome = BASEURL+"homepictures/showhealthknowledge";
+    //登录
+    public  static String   LOGIN=URL+"JianKangChanYe/user/login.do?";
+    //注册验证码
+    public  static String   REGISTER_CODE=URL+"JianKangChanYe/user/register?";
+    //注册
+    public  static String   REGISTER=URL+"JianKangChanYe/user/regiontoamaster?";
+    //忘记密码验证码
+    public  static String   LOSTPASSWORD_CODE=URL+"JianKangChanYe/user/forgetpassword?";
+    //忘记密码完成
+    public  static String   SUBMIT=URL+"JianKangChanYe/user/dauthentication?";
 
 }
