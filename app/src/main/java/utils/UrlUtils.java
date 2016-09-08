6117c9dfc92e24d89dd8e43d148acd71960b9e92
xuan123 <1044072796@qq.com>
2016-09-08 15:20:46 +0800

    轩：我的页面

--- app/src/main/java/utils/UrlUtils.java
+++ app/src/main/java/utils/UrlUtils.java
@@ -7,21 +7,21 @@ import android.os.Environment;
  */
 public class UrlUtils {
     public static String ImageCachePath = Environment.getExternalStorageDirectory().getPath()+"/BigHealt/ImageCache/";// sd路径
-    public  static String   BASEURL="http://showtest.dancw.com";
 
-    public static String URL="http://192.168.0.43:8080/";
+    public static String URL="http://192.168.0.43:8080/JianKangChanYe";
     //-------------------------------API接口----------------------------------
-    public static String CommonHome=BASEURL+"/homepictures/sickness";
-    public static String SpecialHome = BASEURL+"homepictures/showhealthknowledge";
+    public static String CommonHome=URL+"/homepictures/sickness";
+    public static String SpecialHome = URL+"homepictures/showhealthknowledge";
     //登录
-    public  static String   LOGIN=URL+"JianKangChanYe/user/login.do?";
+    public  static String   LOGIN=URL+"/user/login.do?";
     //注册验证码
-    public  static String   REGISTER_CODE=URL+"JianKangChanYe/user/register?";
+    public  static String   REGISTER_CODE=URL+"/user/register?";
     //注册
-    public  static String   REGISTER=URL+"JianKangChanYe/user/regiontoamaster?";
+    public  static String   REGISTER=URL+"/user/regiontoamaster?";
     //忘记密码验证码
-    public  static String   LOSTPASSWORD_CODE=URL+"JianKangChanYe/user/forgetpassword?";
+    public  static String   LOSTPASSWORD_CODE=URL+"/user/forgetpassword?";
     //忘记密码完成
-    public  static String   SUBMIT=URL+"JianKangChanYe/user/dauthentication?";
-
+    public  static String   SUBMIT=URL+"/user/dauthentication?";
+    //医疗养生加盟
+    public static  String LEAGUE = URL+"/hathappenedat/savehealthcare";
 }
