e988465eec33168176bfe8ac03b3f7f9f5bc6ab4
liuyongzhen <mhysa123@hotmail.com>
2016-09-09 13:45:14 +0800

    刘大魔王  预约xml

--- app/src/main/java/dbighealth/bighealth/fragment/home/SpecialFragment.java
+++ app/src/main/java/dbighealth/bighealth/fragment/home/SpecialFragment.java
@@ -69,7 +69,7 @@ public class SpecialFragment extends Fragment implements BaseAdapter.OnItemClick
      *
      */
    public void  initInternet(){
-       Log.e("mhysa","特殊页的地址"+UrlUtils.SpecialHome);
+       Log.e("mhysa12345","特殊页的地址"+UrlUtils.SpecialHome);
 
        OkHttpUtils.get()
                   .url(UrlUtils.SpecialHome)
